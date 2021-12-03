6b4bb085ecee496ac55ec7bf9788287f40b613b7
dlopez144 <dlopez14@uncc.edu>
2021-12-02 22:58:44 -0500

    Fixed an issue where graphs were missing countries

--- scripts/water-barchart-generator.py
+++ scripts/water-barchart-generator.py
@@ -2,14 +2,17 @@ from numpy import nan
 import pandas as pd
 import plotly.offline as pyo
 import plotly.graph_objs as go
-
+import data_cleaner as dc
 
 def gen_barchart(continent, path):
     # Load data
     df = pd.read_csv('../Datasets/SafelyManagedDrinkingWater.csv')
 
+    # Adding continent column to dataframe
+    df = dc.add_continent_col(df)
+
     # Select continent
-    df = df[df.ParentLocation == continent]
+    df = df[df.Continent == continent]
 
     # Leave only one row for each country in the selected continent
     filtered_df = df.drop_duplicates('Location', keep='first')
@@ -42,12 +45,12 @@ def gen_barchart(continent, path):
 
 
 jobs = [
-    ["Europe", '../graphs/europe/europe-water.html'],
-    ["South-East Asia", '../graphs/asia/asia-water.html'],
-    ["Americas", '../graphs/north-america/north-america-water.html'],
-    ["Africa", '../graphs/africa/africa-water.html'],
-    ["Western Pacific", '../graphs/oceania/oceania-water.html'],
-    ["Americas", '../graphs/south-america/south-america-water.html']
+    ["EU", '../graphs/europe/europe-water.html'],
+    ["AS", '../graphs/asia/asia-water.html'],
+    ["NA", '../graphs/north-america/north-america-water.html'],
+    ["AF", '../graphs/africa/africa-water.html'],
+    ["OC", '../graphs/oceania/oceania-water.html'],
+    ["SA", '../graphs/south-america/south-america-water.html']
 ]
 
 for j in jobs:
