39292045e8cd40a36e37f6fa3d9dbf96b7240a51
lukesnc <lukeasimone@gmail.com>
2021-12-02 22:06:51 -0500

    Water barchart working

--- scripts/water-barchart-generator.py
+++ scripts/water-barchart-generator.py
@@ -1,3 +1,4 @@
+from numpy import nan
 import pandas as pd
 import plotly.offline as pyo
 import plotly.graph_objs as go
@@ -6,20 +7,30 @@ import plotly.graph_objs as go
 df = pd.read_csv('../Datasets/SafelyManagedDrinkingWater.csv')
 
 # Select continent
-df = df[df.ParentLocation == 'Europe']
+df = df[df.ParentLocation == 'Americas']
 
-# Sort from lowest to highest
-filtered_df = df.sort_values(by=['Location'])
+# Leave only one row for each country in the selected continent
+filtered_df = df.drop_duplicates('Location', keep='first')
 
-#TODO Get an average ['Value'] for each country, as they're currently split into regions/urban/suburban/etc.
+# Average a specific country's access to water
+avg_df = {}
+for country in filtered_df['Location']:
+    country_df = df[df.Location == country]
+    avg_df[country] = country_df['Value'].mean()
 
-# Preparing data
-data = [go.Bar(
-        x=filtered_df['Location'],
-        y=filtered_df['Value'] # Not averaged yet
-)]
+# Sort by descending order of % pop with clean water
+sorted_df = dict(sorted(avg_df.items(), key=lambda x: x[1], reverse=True))
 
-# Preparing layout
+# Remove countries with no data
+sorted_df = {k: v for k, v in sorted_df.items() if v is not nan}
+
+# Data
+data = [
+    go.Bar(x=list(sorted_df.keys()),
+           y=list(sorted_df.values()))
+]
+
+# Layout
 layout = go.Layout(title='safety_managed_drinking_water', xaxis_title="Countries",
                    yaxis_title="Population using safely managed drinking-water services (%)")
 
