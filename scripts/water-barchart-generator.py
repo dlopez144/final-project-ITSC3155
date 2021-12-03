f5de238f6cbbe5e4da745d2f1893cabae32a1b9d
lukesnc <lukeasimone@gmail.com>
2021-12-02 22:33:02 -0500

    Added html file generation

--- scripts/water-barchart-generator.py
+++ scripts/water-barchart-generator.py
@@ -3,37 +3,52 @@ import pandas as pd
 import plotly.offline as pyo
 import plotly.graph_objs as go
 
-# Load data
-df = pd.read_csv('../Datasets/SafelyManagedDrinkingWater.csv')
 
-# Select continent
-df = df[df.ParentLocation == 'Americas']
+def gen_barchart(continent, path):
+    # Load data
+    df = pd.read_csv('../Datasets/SafelyManagedDrinkingWater.csv')
 
-# Leave only one row for each country in the selected continent
-filtered_df = df.drop_duplicates('Location', keep='first')
+    # Select continent
+    df = df[df.ParentLocation == continent]
 
-# Average a specific country's access to water
-avg_df = {}
-for country in filtered_df['Location']:
-    country_df = df[df.Location == country]
-    avg_df[country] = country_df['Value'].mean()
+    # Leave only one row for each country in the selected continent
+    filtered_df = df.drop_duplicates('Location', keep='first')
 
-# Sort by descending order of % pop with clean water
-sorted_df = dict(sorted(avg_df.items(), key=lambda x: x[1], reverse=True))
+    # Average a specific country's access to water
+    avg_df = {}
+    for country in filtered_df['Location']:
+        country_df = df[df.Location == country]
+        avg_df[country] = country_df['Value'].mean()
 
-# Remove countries with no data
-sorted_df = {k: v for k, v in sorted_df.items() if v is not nan}
+    # Sort by descending order of % pop with clean water
+    sorted_df = dict(sorted(avg_df.items(), key=lambda x: x[1], reverse=True))
 
-# Data
-data = [
-    go.Bar(x=list(sorted_df.keys()),
-           y=list(sorted_df.values()))
+    # Remove countries with no data
+    sorted_df = {k: v for k, v in sorted_df.items() if v is not nan}
+
+    # Data
+    data = [
+        go.Bar(x=list(sorted_df.keys()),
+               y=list(sorted_df.values()))
+    ]
+
+    # Layout
+    layout = go.Layout(title=f'Access to Safe Drinking Water in {continent}', xaxis_title="Countries",
+                       yaxis_title="Population drinking safe water (%)")
+
+    # Write file
+    fig = go.Figure(data=data, layout=layout)
+    pyo.plot(fig, filename=path)
+
+
+jobs = [
+    ["Europe", '../graphs/europe/europe-water.html'],
+    ["South-East Asia", '../graphs/asia/asia-water.html'],
+    ["Americas", '../graphs/north-america/north-america-water.html'],
+    ["Africa", '../graphs/africa/africa-water.html'],
+    ["Western Pacific", '../graphs/oceania/oceania-water.html'],
+    ["Americas", '../graphs/south-america/south-america-water.html']
 ]
 
-# Layout
-layout = go.Layout(title='safety_managed_drinking_water', xaxis_title="Countries",
-                   yaxis_title="Population using safely managed drinking-water services (%)")
-
-
-fig = go.Figure(data=data, layout=layout)
-fig.show()
+for j in jobs:
+    gen_barchart(j[0], j[1])
